530af903df2e9a3a176fc98adc76bf930db4beb8
Mateusz Juraszek <mateusz@webtigers.eu>
2014-07-14 08:57:07 +0200

    finalise form_identifier change

--- lib/clerkapp.rb
+++ lib/clerkapp.rb
@@ -7,44 +7,46 @@ class Clerkapp
   class NoAccount < StandardError; end
   class ApiError < StandardError; end
 
+  attr_accessor :async, :test, :raise_on_error
+
   class << self
     def list(options = {})
-      new.list
+      new(options).list
     end
 
     def print(form_identifier, fields = {}, options = {})
-      raise ArgumentError.new("form_identifier is required") unless form_identifier
-      raise ArgumentError.new("fields must be a hash")  unless fields.is_a?(Hash)
-      raise ArgumentError.new("options must be a hash") unless options.is_a?(Hash)
-      default_options = {
-        "test"           => false,
-        "async"          => false,
-        "raise_on_error" => true
-      }
-      new.print(form_identifier.to_s, fields, default_options.merge(options))
+      new(options).print(form_identifier, fields)
     end
   end
 
-  def initialize
+  def initialize(options = {})
     raise NoAccount, "CLERK_URL is not specified, did you install heroku add-on?" unless ENV['CLERK_URL']
+    raise ArgumentError.new("options must be a hash") unless options.is_a?(Hash)
+
+    @test           = options.fetch("test", false)
+    @async          = options.fetch("async", false)
+    @raise_on_error = options.fetch("raise_on_error", true)
   end
 
-  def print(form_identifier, fields = {}, options = {})
-    options = options.select {|k, v| [:test, :async].include?(k)}
-    raise_on_error = options.delete("raise_on_error")
-    response = http_client.post "/api/printouts", {
-      form_identifier: form_identifier,
-      fields:          fields
-    }.merge(options).to_json
+  def print(form_identifier, fields = {})
+    raise ArgumentError.new("form_identifier is required") unless form_identifier
+    raise ArgumentError.new("fields must be a hash")       unless fields.is_a?(Hash)
 
-    if raise_on_error && !response.success?
+    response = http_client.post "/api/printouts", {
+      "form_identifier" => form_identifier,
+      "fields"          => fields,
+      "test"            => test,
+      "async"           => async
+    }.to_json
+
+    if @raise_on_error && !response.success?
       raise Clerkapp::ApiError.new("status_code: #{response.status}#{"\n#{response.body}" if response.body.present?}")
     end
 
     if !response.success?
       parsed(response)
-    elsif options["async"]
-      parsed(response)['ticket_id']
+    elsif async
+      parsed(response)["ticket_id"]
     elsif block_given? # no need to load to memory
       return_value = nil
       Tempfile.open("clerkapp") do |f|
